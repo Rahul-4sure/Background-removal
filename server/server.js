fcb7ff4f7b4557de7555f5d32730020de119d576
Rahul-4sure <rahulkumar20021010@gmail.com>
2026-03-07 23:17:19 +0530

    some more tweeks

--- server/server.js
+++ server/server.js
@@ -1,20 +1,17 @@
 import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
-import connectDB from './configs/connection.js';
-
-const PORT = process.env.PORT || 4000;
+import connectDB from './configs/connection.js'
 
 const app = express();
 
-await connectDB();
+app.use(cors());
+app.use(express.json());
 
-app.get('/',(req,res)=>{
-    res.send("Hey I am Rahul")
-})
+connectDB();
 
+app.get('/', (req,res)=>{
+    res.send("Hey I am Rahul");
+});
 
-app.use(express.json())
-app.use(cors())
-
-app.listen(PORT,()=>{});
+export default app;
