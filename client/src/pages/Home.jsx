ff7d629e0f12c76b502ed1229b5477c2209acca2
Rahul-4sure <rahulkumar20021010@gmail.com>
2026-03-25 16:29:12 +0530

    minor error

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Header from '../components/Header'
 import Steps from '../components/Steps'
 import BgSlider from '../components/BgSlider'
 import Testimonials from '../components/Testimonials'
-import Seemagic from '../components/Seemagic'
+import Seemagic from '../components/SeeMagic.jsx'
 
 const Home = () => {
   return (
